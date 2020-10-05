2a803c9ef3e8c602bfbb7a5d1dffd6a81ae02afe
Erik Dubois <erik.dubois@gmail.com>
2020-10-05 08:33:26 +0200

    Bibata-Modern-Ice

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name=arcolinux-dev
-iso_label="arcolinux-dev-v20.11.5"
+iso_name=arcolinux-git
+iso_label="arcolinux-git-v20.11.5"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
 iso_version="v20.11.5"
